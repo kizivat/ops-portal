0ff572f3d68f8173e49ab47a0664600e9a503509
Jan Suchal <jan.suchal@gmail.com>
2024-11-11 09:27:19 +0100

    Fix tests

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,14 +1,14 @@
 require "test_helper"
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
-  test "ticket-updated should receive 400 without x-hub-signature header" do
+  test "ticket-updated should receive 401 without x-hub-signature header" do
     post webhooks_ticket_updated_url
-    assert_response :bad_request
+    assert_response :unauthorized
   end
 
   test "ticket-updated should receive 400 with bad signature" do
     post webhooks_ticket_updated_url, params: {}, headers: { "X-Hub-Signature": "sha1=sadfasdfas" }
-    assert_response :bad_request
+    assert_response :forbidden
   end
 
   test "ticket-updated should successfuly update existing issue" do
