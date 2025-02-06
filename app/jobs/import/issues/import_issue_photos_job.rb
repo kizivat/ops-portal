6b78cec4abccae0233645767be2f328fb1e8d35e
luciajanikova <19lucia99@gmail.com>
2025-02-06 13:22:30 +0100

    Add position to Issue photos

--- app/jobs/import/issues/import_issue_photos_job.rb
+++ app/jobs/import/issues/import_issue_photos_job.rb
@@ -6,15 +6,8 @@ module Import
       Legacy::GenericModel.set_table_name("media_images")
       Legacy::GenericModel.where(alert_id: issue.id).find_in_batches do |group|
         group.each do |legacy_record|
-          issue.photos.attach(io: download_from_ops_portal(legacy_record.href), filename: File.basename(legacy_record.href))
-
-          # issue.images.find_or_create_by!(
-          #   id: legacy_record.id,
-          #   original: legacy_record.original,
-          #   path: legacy_record.href,
-          #   position: legacy_record.position,
-          #   thumbnail: legacy_record.thumbnail
-          # )
+          issue.photos.attach(io: download_from_ops_portal(legacy_record.original), filename: File.basename(legacy_record.original))
+          issue.photos.order(:id).last.update(position: legacy_record.position)
         end
       end
     end
