92887e2e5f02f6d858016ad87d4650d2fe2f283e
Marek Ceľuch <celuchmarek@gmail.com>
2025-02-13 09:02:02 +0100

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_11_091245) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_13_074949) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
@@ -79,6 +79,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_11_091245) do
     t.string "webhook_public_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "triage_user_id"
   end
 
   create_table "connector_users", force: :cascade do |t|
