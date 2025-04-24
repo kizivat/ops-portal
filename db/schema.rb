23303cdd1ce965b3e26ccf1ca47b944bfa0f2589
Jano Suchal <jan.suchal@slovensko.digital>
2025-04-24 11:06:28 +0200

    Add support for geosearch, refs #102 (#324)
    Add support for geosearch, refs #102

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_15_181958) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_24_072330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
+  enable_extension "postgis"
   enable_extension "unaccent"
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -255,6 +256,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_15_181958) do
     t.string "address_country_code"
     t.string "address_district"
     t.integer "resolution_external_id"
+    t.index "((st_point(longitude, latitude, 4326))::geography)", name: "index_issues_on_location", using: :gist
     t.index ["author_id"], name: "index_issues_on_author_id"
     t.index ["category_id"], name: "index_issues_on_category_id"
     t.index ["legacy_id"], name: "index_issues_on_legacy_id", unique: true
