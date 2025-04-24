51e61aa68d6bc029800864c50dab65f3b21c21da
luciajanikova <19lucia99@gmail.com>
2025-04-24 12:59:57 +0200

    Lint

--- db/migrate/20250424082256_add_display_name_to_users.rb
+++ db/migrate/20250424082256_add_display_name_to_users.rb
@@ -4,10 +4,10 @@ class AddDisplayNameToUsers < ActiveRecord::Migration[8.0]
 
     User.find_each do |user|
       display_name = if user.anonymous?
-                       "Anonym #{user.id}"
-                     else
-                       [ user.firstname, user.lastname ].reject(&:blank?).join(" ")
-                     end
+       "Anonym #{user.id}"
+      else
+       [ user.firstname, user.lastname ].reject(&:blank?).join(" ")
+      end
 
       user.update!(display_name: display_name)
     end
--- db/migrate/20250424082339_add_display_name_to_legacy_agents.rb
+++ db/migrate/20250424082339_add_display_name_to_legacy_agents.rb
@@ -4,10 +4,10 @@ class AddDisplayNameToLegacyAgents < ActiveRecord::Migration[8.0]
 
     Legacy::Agent.find_each do |agent|
       display_name = if agent.anonymous?
-                       "Anonymný agent #{agent.id}"
-                     else
-                       [ agent.firstname, agent.lastname ].reject(&:blank?).join(" ")
-                     end
+       "Anonymný agent #{agent.id}"
+      else
+       [ agent.firstname, agent.lastname ].reject(&:blank?).join(" ")
+      end
       agent.update!(display_name: display_name)
     end
   end
