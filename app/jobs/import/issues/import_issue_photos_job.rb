f6fc490995db348613d7e7266afb76bfb3c6af32
luciajanikova <19lucia99@gmail.com>
2025-05-07 23:05:14 +0200

    Lint

--- app/jobs/import/issues/import_issue_photos_job.rb
+++ app/jobs/import/issues/import_issue_photos_job.rb
@@ -2,7 +2,7 @@ module Import
   class Issues::ImportIssuePhotosJob < ApplicationJob
     include ImportMethods
 
-    def perform(issue:, import_photo_job: Issues::ImportIssuePhotoJob )
+    def perform(issue:, import_photo_job: Issues::ImportIssuePhotoJob)
       Legacy::Alerts::Image.where(alert_id: issue.legacy_id).order(:position).find_in_batches do |group|
         group.each do |legacy_record|
           import_photo_job.perform_later(legacy_record, issue: issue)
