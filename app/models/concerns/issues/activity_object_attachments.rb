52593dc54f8d58898d8542d98f1c022c6baf29aa
Jan Suchal <jan.suchal@gmail.com>
2025-05-11 11:12:21 +0200

    Add zoombox

--- app/models/concerns/issues/activity_object_attachments.rb
+++ app/models/concerns/issues/activity_object_attachments.rb
@@ -4,6 +4,7 @@ module Issues
 
     included do
       has_many_attached :attachments do |photo|
+        photo.variant :full, resize_to_limit: [ 1280, 960 ], preprocessed: true
         photo.variant :thumb, resize_to_limit: [ 320, 240 ], preprocessed: true
       end
     end
