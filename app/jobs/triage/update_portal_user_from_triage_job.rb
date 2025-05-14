d57b84f7f94f185dc63c88c9f8d2de69a11776a1
Marek Ceľuch <celuchmarek@gmail.com>
2025-05-14 23:14:15 +0200

    do not raise in UpdatePortalUserFromTriageJob if user not found

--- app/jobs/triage/update_portal_user_from_triage_job.rb
+++ app/jobs/triage/update_portal_user_from_triage_job.rb
@@ -3,7 +3,7 @@ class Triage::UpdatePortalUserFromTriageJob < ApplicationJob
     triage_user = triage_zammad_client.find_user(user_id)
     return unless triage_user
 
-    user = User.find_by!(external_id: triage_user.id.to_i)
+    user = User.find_by(external_id: triage_user.id.to_i)
     return unless user
 
     user.update!(banned: triage_user.banned) unless triage_user.banned == user.banned
