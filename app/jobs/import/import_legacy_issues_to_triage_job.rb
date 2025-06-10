a4dfe3a85bb835dd369615f541b72518dbc553a4
luciajanikova <19lucia99@gmail.com>
2025-06-10 09:59:12 +0200

    Update query

--- app/jobs/import/import_legacy_issues_to_triage_job.rb
+++ app/jobs/import/import_legacy_issues_to_triage_job.rb
@@ -3,14 +3,10 @@ module Import
     queue_with_priority 100
 
     def perform(municipality:, municipality_district: nil, import_issue_job: ::SyncIssueToTriageJob)
-      conditions = {
-        municipality: municipality
-      }
-      conditions.merge!(municipality_district: municipality_district) if municipality_district
+      scope = Issue.where.not(legacy_id: nil).where(municipality: municipality)
+      scope = scope.where(municipality_district: municipality_district) if municipality_district
 
-      Issue
-        .where.not(legacy_id: nil)
-        .where(**conditions).find_in_batches do |group|
+      scope.find_in_batches do |group|
         group.each do |issue|
           import_issue_job.perform_later(issue, import: true)
         end
