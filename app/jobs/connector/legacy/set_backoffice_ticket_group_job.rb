154e5ead90c25657b5e447d616110a0fd857238f
luciajanikova <19lucia99@gmail.com>
2025-06-11 15:06:01 +0200

    Lint

--- app/jobs/connector/legacy/set_backoffice_ticket_group_job.rb
+++ app/jobs/connector/legacy/set_backoffice_ticket_group_job.rb
@@ -4,7 +4,7 @@ class Connector::Legacy::SetBackofficeTicketGroupJob < ApplicationJob
 
     issue = Issue.find_by(resolution_external_id: triage_issue_id)
 
-    legacy_org_units = ResponsibleSubjects::OrganizationUnit.where(legacy_id: [issue.legacy_data&.fetch("organizational_unit_id"), issue.legacy_data&.fetch("organization_unit_id2")])
+    legacy_org_units = ResponsibleSubjects::OrganizationUnit.where(legacy_id: [ issue.legacy_data&.fetch("organizational_unit_id"), issue.legacy_data&.fetch("organization_unit_id2") ])
 
     return unless legacy_org_units.any?
 
@@ -12,7 +12,7 @@ class Connector::Legacy::SetBackofficeTicketGroupJob < ApplicationJob
       legacy_org_units.find(issue.backoffice_owner.organization_unit_id)
     else
       legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organizational_unit_id")).presence || legacy_org_units.find_by(legacy_id: issue.legacy_data&.fetch("organization_unit_id2"))
-   end
+    end
 
     group = zammad_client.find_or_create_group(selected_organization_unit.name)
     zammad_client.add_ticket_to_group(issue, group.name)
