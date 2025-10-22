4f75e23fc4e2ad217808833eddad3e2ba4294e73
Marek Ceľuch <celuchmarek@gmail.com>
2025-10-22 15:38:31 +0200

    simplify started_at backfill job

--- app/jobs/issues/backfill_resolution_started_at_enqueue_job.rb
+++ app/jobs/issues/backfill_resolution_started_at_enqueue_job.rb
@@ -1,14 +1,9 @@
 class Issues::BackfillResolutionStartedAtEnqueueJob < ApplicationJob
   queue_as :default
 
-  DEFAULT_BATCH_SIZE = 500
-
-  def perform(batch_size: DEFAULT_BATCH_SIZE)
-    scope = Issue.where.not(resolution_external_id: nil).where(resolution_started_at: nil).select(:id)
-    return unless scope.exists?
-
-    scope.in_batches(of: batch_size) do |relation|
-      Issues::BackfillResolutionStartedAtBatchJob.perform_later(issue_ids: relation.ids)
+  def perform(child_job_queue: :default)
+    Issue.where.not(resolution_external_id: nil).where(resolution_started_at: nil).find_each do |issue|
+      Issues::BackfillResolutionStartedAtJob.set(queue: child_job_queue).perform_later(issue)
     end
   end
 end
