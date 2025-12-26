a8059f4d735d269b57a86ec54bbb4c11394c99c3
Jan Suchal <jan.suchal@gmail.com>
2025-12-26 15:38:47 +0100

    Add `none` class as a dynamic Stimulus class in autocomplete filter

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -1,14 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["input", "item", "list"]
+    static classes = ["none"]
 
     filter() {
         const query = this.normalizeString(this.inputTarget.value.trim())
         const items = this.itemTargets
 
         if (query === "") {
-            items.forEach((el) => el.classList.remove("none"))
+            items.forEach((el) => el.classList.remove(this.noneClass))
             return
         }
 
@@ -17,11 +18,11 @@ export default class extends Controller {
             const score = this.matchScore(label, query)
 
             el.dataset.score = score
-            el.classList.toggle("none", score === Infinity)
+            el.classList.toggle(this.noneClass, score === Infinity)
         })
 
         const visible = items
-            .filter((el) => !el.classList.contains("none"))
+            .filter((el) => !el.classList.contains(this.noneClass))
             .sort((a, b) => a.dataset.score - b.dataset.score)
 
         visible.forEach((el) => this.listTarget.appendChild(el))
@@ -30,7 +31,7 @@ export default class extends Controller {
     enter(event) {
         event.preventDefault()
 
-        this.itemTargets.find(el => !el.classList.contains("none"))
+        this.itemTargets.find(el => !el.classList.contains(this.noneClass))
             ?.querySelector("a")
             ?.click()
     }
