79ce4409185d12bd77752f26dcd71dd2cf98cce6
Jozef Steinhübl <contact@xhyrom.dev>
2025-12-29 16:09:58 +0100

    feat(draft): add issue reference

--- db/structure.sql
+++ db/structure.sql
@@ -887,7 +887,8 @@ CREATE TABLE public.issues_drafts (
     address_suburb character varying,
     zoom integer,
     issue_type character varying DEFAULT 'issue'::character varying NOT NULL,
-    duplicates_shown boolean DEFAULT false NOT NULL
+    duplicates_shown boolean DEFAULT false NOT NULL,
+    issue_id bigint
 );
 
 
@@ -4132,6 +4133,7 @@ ALTER TABLE ONLY public.cms_categories
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20251229150246'),
 ('20251226102461'),
 ('20251226102460'),
 ('20251226102459'),
